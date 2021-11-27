8d3e52eacc0354f387b1bf3f6761843f8c039ddd
Paul Xiong <paul.xiong.2007@gmail.com>
2021-11-26 22:09:18 -0800

    removed Upload, etc... works well.

--- app1/src/App.vue
+++ app1/src/App.vue
@@ -1,43 +1,21 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Yourrrrr Vue.js App"/>
-  <B_import msg="Welcome to Myyyyyyyy Vue.js App"/>
-  <UpLoad msg= "Upload your file"/>
-    <VueFileAgent
-      :uploadUrl="'https://www.mocky.io/v2/5d4fb20b3000005c111099e3'"
-      :uploadHeaders="{}"
-      :multiple="true"
-      :deletable="true"
-      :editable="true"
-      :meta="true"
-      :accept="'image/*,video/*,.pdf,.zip'"
-      :maxSize="'14MB'"
-      :maxFiles="8"
-      :helpText="'Select files'"
-      :errorText="{
-        type: 'Please select images, videos, pdf or zip files',
-        size: 'You selected a larger file!',
-      }"
-      :thumbnailSize="120"
-      :theme="'list'"
-      v-model="fileRecords"
-    ></VueFileAgent>
-
+  <button @click="A_method1">Click A_method</button>
+  <B_import ref="B" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-import UpLoad from './components/UpLoad.vue'
 import B_import from './components/fileB.vue'
-
-
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
-    UpLoad,
     B_import
+  },
+  methods: {
+    A_method1() {
+      this.$refs.B.method1()
+    }
   }
 }
 </script>
